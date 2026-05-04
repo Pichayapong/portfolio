d1c95e9ec129f427341664dcefc2555b37c35202
PTK <pichayapong.t@gmail.com>
2026-05-04 17:30:55 +0700

    fix: default lang en

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import { routing } from './i18n/routing'
 
 const handler = createMiddleware(routing)
 
-export { handler as proxy }
+export default handler
 
 export const config = {
   matcher: ['/((?!api|_next|_vercel|.*\\..*).*)', '/'],
